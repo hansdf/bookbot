87c5da73b57470474c663fa747af1bd35481936a
hansdf <dornsback@gmail.com>
2024-06-04 14:12:31 -0300

    Update main.py
    adding word counter function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(file_contents)
+
+def main():
+    def num_of_words():
+        words = file_contents.split()
+        counter = 0
+        for word in words:
+            counter += 1
+        print(f"This text file contains {counter} words.")
+
+    num_of_words()
+        
+
+main()
